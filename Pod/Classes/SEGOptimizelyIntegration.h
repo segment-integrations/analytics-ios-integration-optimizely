1ededaf68c8b28a50aa6fad2074edc0b889b5669
Brie <brienne.mcnally@segment.com>
2020-10-05 18:44:54 -0600

    Update namespacing

--- Pod/Classes/SEGOptimizelyIntegration.h
+++ Pod/Classes/SEGOptimizelyIntegration.h
@@ -1,5 +1,10 @@
 #import <Foundation/Foundation.h>
+
+#if defined(__has_include) && __has_include(<Analytics/Analytics.h>)
 #import <Analytics/SEGIntegration.h>
+#else
+#import <Segment/SEGIntegration.h>
+#endif
 
 @interface SEGOptimizelyIntegration : NSObject<SEGIntegration>
 
